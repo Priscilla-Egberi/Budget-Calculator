2cc7f7b2f801b315d71b73dcbb0969a0a1ba1bb1
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-12-09 13:44:53 +0100

    We just finished working on the total spending function and it is working very fine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import {useState} from "react"
 import './App.css';
+import Alert from "./components/Alert"
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from "./components/ExpenseList"
+
+const initialExpenses = [
+  {id:0, charge:"payment for bills", amount:1200},
+  {id:1, charge:"School equipments", amount:2000},
+  {id:2, charge:"purchasement of laptop", amount:12000}
+]
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses)
   return (
-    <div className="App">
-      <p>my template</p>
-    </div>
+    <>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList />
+      </main>
+      <h1>total spending:{" "}
+      <span className="total">$
+      {expenses.reduce((acc, curr)=>{
+        return acc +=curr.amount
+      },0)}
+      </span>
+      </h1>
+    </>
   );
 }
 
