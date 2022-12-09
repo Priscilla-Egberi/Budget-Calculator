19dc53497667a67b0fdba211376e210600bb54ac
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-12-09 14:47:28 +0100

    ExpenseForm: added the two inputs for collecting charge and amount. I think we are done with the design, next would be the functions

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import {MdSend} from "react-icons/md"
 
 export default function ExpenseForm() {
   return (
-    <div>ExpenseForm</div>
+    <form>
+        <div className="form-center">
+            <div className="form-group">
+                <label htmlFor="charge">charge</label>
+                <input className='form-control' type="text" id="charge" name="charge" placeholder="e.g. rent" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="amount">amount</label>
+                <input className='form-control' type="text" id="amount" name="amount" placeholder="e.g. 100" />
+            </div>
+        </div>
+        <button type="submit" className='btn'>submit<MdSend className="btn-icon" /></button>
+    </form>
   )
 }
